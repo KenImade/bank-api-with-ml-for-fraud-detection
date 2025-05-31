3b9f40d5caf383833577de05a9bf3bcb039b3f4b
Kenneth Imade <kenneth.imade@yahoo.com>
2025-05-31 04:28:06 +0100

    chore: add bank account enums, schema and models

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -11,6 +11,7 @@ from backend.app.auth.schema import BaseUserSchema, RoleChoicesSchema
 if TYPE_CHECKING:
     from backend.app.user_profile.models import Profile
     from backend.app.next_of_kin.models import NextOfKin
+    from backend.app.bank_account.models import BankAccount
 
 
 class User(BaseUserSchema, table=True):
@@ -52,6 +53,7 @@ class User(BaseUserSchema, table=True):
     )
 
     next_of_kins: list["NextOfKin"] = Relationship(back_populates="user")
+    bank_accounts: list["BankAccount"] = Relationship(back_populates="user")
 
     @computed_field
     @property
